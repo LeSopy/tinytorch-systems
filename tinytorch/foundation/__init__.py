307d0a87d511a6fe3cbb9f3bcf2bfdede1ef8881
sopy <souopyves@gmail.com>
2026-07-23 02:06:40 -0400

    feat(examples): Add MLP digit classifier trained on Tinydigits (85%+ accuracy)

---
+++ tinytorch/foundation/__init__.py
@@ -0,0 +1,5 @@
+from tinytorch.foundation.tensor import Tensor
+from tinytorch.foundation.layers import Linear, ReLU, Dropout, Sequential
+from tinytorch.foundation.losses import CrossEntropyLoss, MSELoss
+from tinytorch.foundation.optimizers import SGD, Adam, AdamW
+from tinytorch.foundation.autograd import enable_autograd
